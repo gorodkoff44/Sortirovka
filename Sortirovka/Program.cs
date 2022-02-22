2322f74867e3304d4eebba58d4bd7a97fbc17d75
Кэт Студент <studket44@outlook.com>
2022-02-22 08:46:28 +0300

    Добавьте файлы проекта.

---
+++ Sortirovka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortirovka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+            var selectedPeople = new List<string>();
+            foreach (string person in people)
+            {
+                if (person.ToUpper().StartsWith("T"))
+                    selectedPeople.Add(person);
+            }
+            selectedPeople.Sort();
+            foreach (string person in selectedPeople)
+                Console.WriteLine(person);
+            Console.ReadKey();
+        }
+    }
+}
